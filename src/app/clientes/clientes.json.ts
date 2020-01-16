8919fd635bfbb98a9c8966afd2c813a81d7d03e2
fjimenezJOB <fjimenezjob@gmail.com>
2020-01-16 10:57:06 +0100

    vista de clientes con json desacoplado

---
+++ src/app/clientes/clientes.json.ts
@@ -0,0 +1,9 @@
+import { Cliente } from "./cliente";
+
+export const CLIENTES: Cliente[] = [
+    {id:1, nombre: 'Fran' , apellido:'Jimenez', email: 'fjimenezjob@gmail.com', createdAt: '2020-01-16'},
+    {id:2, nombre: 'Petro' , apellido:'TEST', email: 'petroTest@gmail.com', createdAt: '2020-01-16'},
+    {id:3, nombre: 'Johan' , apellido:'Doe', email: 'jdoe@gmail.com', createdAt: '2020-01-16'},
+    {id:4, nombre: 'Alejandro' , apellido:'Test', email: 'atest@gmail.com', createdAt: '2020-01-16'},
+    {id:5, nombre: 'Jorge' , apellido:'Martinez', email: 'jmartinez@gmail.com', createdAt: '2020-01-16'}
+  ];
