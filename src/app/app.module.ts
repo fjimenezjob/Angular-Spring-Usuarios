7f349dcb0326f8c7e0ac4b7f48d3c4494afd7df9
fjimenezJOB <fjimenezjob@gmail.com>
2020-01-16 11:22:07 +0100

    added clienteService

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { DirectivaComponent } from './directiva/directiva.component';
 import { ClientesComponent } from './clientes/clientes.component';
+import { ClienteService } from './clientes/cliente.service';
 
 @NgModule({
   declarations: [
@@ -13,12 +14,13 @@ import { ClientesComponent } from './clientes/clientes.component';
     HeaderComponent,
     FooterComponent,
     DirectivaComponent,
-    ClientesComponent
+    ClientesComponent,
+    
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [ClienteService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/clientes/cliente.service.ts
+++ src/app/clientes/cliente.service.ts
@@ -1,9 +1,13 @@
-import { Injectable } from '@angular/core';
+import { Injectable} from '@angular/core';
+import { CLIENTES } from "./clientes.json";
+import { Cliente } from './cliente';
 
-@Injectable({
-  providedIn: 'root'
-})
+
+@Injectable()
 export class ClienteService {
-
   constructor() { }
+
+  getClientes(): Cliente[] {
+    return CLIENTES; 
+  }
 }
