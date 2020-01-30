c44d0d9846835aa71a4bff81707e8df6570340c9
franjimenez.it <franjimenez@hotelbeds.com>
2020-01-30 11:38:28 +0100

    validacion  formulario cliente ok

--- src/app/clientes/cliente.service.ts
+++ src/app/clientes/cliente.service.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@angular/core';
-import { CLIENTES } from "./clientes.json";
 import { Cliente } from './cliente';
 import { of, Observable } from 'rxjs';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
@@ -13,7 +12,6 @@ export class ClienteService {
   constructor(private http: HttpClient) { }
 
   getClientes(): Observable<Cliente[]> {
-    // return of(CLIENTES); 
     return this.http.get<Cliente[]>(this.urlEndpoint);
   }
 
@@ -21,16 +19,16 @@ export class ClienteService {
     return this.http.post<Cliente>(this.urlEndpoint, cliente, { headers: this.httpHeaders })
   }
 
-  getCliente(id): Observable<Cliente>{
-    
+  getCliente(id): Observable<Cliente> {
+
     return this.http.get<Cliente>(`${this.urlEndpoint}/${id}`)
   }
 
-  update(cliente:  Cliente) : Observable<Cliente>{
-    return this.http.put<Cliente>(`${this.urlEndpoint}/${cliente.id}`, cliente, {headers: this.httpHeaders})
+  update(cliente: Cliente): Observable<Cliente> {
+    return this.http.put<Cliente>(`${this.urlEndpoint}/${cliente.id}`, cliente, { headers: this.httpHeaders })
   }
- 
-  delete (id : number) : Observable<Cliente> {
-    return this.http.delete<Cliente>(`${this.urlEndpoint}/${id}`, {headers: this.httpHeaders})
+
+  delete(id: number): Observable<Cliente> {
+    return this.http.delete<Cliente>(`${this.urlEndpoint}/${id}`, { headers: this.httpHeaders })
   }
 }
